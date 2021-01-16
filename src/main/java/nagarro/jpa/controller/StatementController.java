f86b4be5b9981f352bbf10ddb4e97d7bc516d0fc
tonyukuk <alperkopuz@gmail.com>
2021-01-16 22:05:09 +0300

    UI has been completed

--- src/main/java/nagarro/jpa/controller/StatementController.java
+++ src/main/java/nagarro/jpa/controller/StatementController.java
@@ -11,6 +11,7 @@ import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -48,15 +49,38 @@ public class StatementController {
 
     @PostMapping("/findStatements")
     public String findStatements(@Valid StatementQuery statementQuery, BindingResult result, Model model) throws ParseException {
-/*
+
         if (result.hasErrors()) {
-            return "add-user";
+            return "statementInvestigation";
         }
 
-        userRepository.save(user);
-        return "redirect:/index";
+        if (statementQuery.getFromDate() == null && statementQuery.getToDate() != null) {
+            FieldError error = new FieldError("fromDate", "fromDate",
+                    "Both Date Fields should be filled ");
+            result.addError(error);
+            return "statementInvestigation";
+        }
 
- */
+        if (statementQuery.getFromDate() != null && statementQuery.getToDate() == null) {
+            FieldError error = new FieldError("toDate", "toDate",
+                    "Both Date Fields should be filled ");
+            result.addError(error);
+            return "statementInvestigation";
+        }
+
+        if (statementQuery.getFromBalance() != null && statementQuery.getToBalance() == null) {
+            FieldError error = new FieldError("toBalance", "toBalance",
+                    "Both Balance Fields should be filled ");
+            result.addError(error);
+            return "statementInvestigation";
+        }
+
+        if (statementQuery.getFromBalance() == null && statementQuery.getToBalance() != null) {
+            FieldError error = new FieldError("fromBalance", "fromBalance",
+                    "Both Balance Fields should be filled ");
+            result.addError(error);
+            return "statementInvestigation";
+        }
 
         List<Statement> statementList = findStatements(statementQuery.getAccountId(), statementQuery.getFromDate(), statementQuery.getToDate(),
                 statementQuery.getFromBalance(), statementQuery.getToBalance());
@@ -70,50 +94,44 @@ public class StatementController {
     public List<Statement> findStatements(String accountId, Date fromDate, Date toDate, BigDecimal fromAmount, BigDecimal toAmount)
             throws ParseException { //TODO exception please
 
-        //TODO accountID boş ise hemen exception fırlat hatta bunu ekrana yap
 
         List<Statement> statementList = statementRepository.findStatementsByAccountId(accountId);
-        if (fromDate == null && toDate == null && fromAmount == null && toAmount == null) {  //TODO null kısımları bi test et
+        if (fromDate == null && fromAmount == null) {
             return statementList;
         }
 
         List<Statement> filteredList = new ArrayList<>();
 
-        //TODO fromDate dolu ama toDate boşs ise error fırlat ikisinin de dolu olması lazım olmadı bunları ön yüzde hallet
 
         //TODO tarihler doğru girilmemişse hemen hata fırlat ikisinin de dolu olması lazım  olmadı bunları ön yüzde hallet
 
         //TODO fromBalance to Balance doğru girilmişmi falan bu kontrolleri yap
 
 
-        if (fromDate != null || fromAmount != null) {
-            for (Statement statement : statementList) { //TODO tell the reason why you are doing it like that
+        for (Statement statement : statementList) { //TODO tell the reason why you are doing it like that
 
 
-                if (fromDate != null && fromAmount == null) {
-                    DateFormat formatter = new SimpleDateFormat("dd.MM.yyyy"); //TODO move me to util method
-                    Date date = formatter.parse(statement.getDateField());
-                    if (date.before(toDate) && (date.after(fromDate) || date.equals(fromDate))) { //TODO describe it
-                        filteredList.add(statement);  //TODO please think about all exceptions that might occure there
-                        //TOO please think about if user writes wrong dates try to throw them at frontend !!
-                    }
-                } else if (fromAmount != null && fromDate == null) {
-
-                    if (new BigDecimal(statement.getAmount()).compareTo(fromAmount) == 1 &&
-                            toAmount.compareTo(new BigDecimal(statement.getAmount())) == 1) {
-                        filteredList.add(statement);  //TODO please think about all exceptions that might occure there
-                    }
-                } else if (fromAmount != null && fromDate != null) {
-                    DateFormat formatter = new SimpleDateFormat("dd.MM.yyyy"); //TODO move me to util method
-                    Date date = formatter.parse(statement.getDateField());
-                    if (date.before(toDate) && (date.after(fromDate) || date.equals(fromDate)) &&
-                            new BigDecimal(statement.getAmount()).compareTo(fromAmount) == 1 &&
-                            toAmount.compareTo(new BigDecimal(statement.getAmount())) == 1) { //TODO describe it
-                        filteredList.add(statement);  //TODO please think about all exceptions that might occure there
-                    }
+            if (fromDate != null && fromAmount == null) {
+                DateFormat formatter = new SimpleDateFormat("dd.MM.yyyy"); //TODO move me to util method
+                Date date = formatter.parse(statement.getDateField());
+                if (date.before(toDate) && (date.after(fromDate) || date.equals(fromDate))) { //TODO describe it
+                    filteredList.add(statement);  //TODO please think about all exceptions that might occure there
+                    //TOO please think about if user writes wrong dates try to throw them at frontend !!
                 }
+            } else if (fromAmount != null && fromDate == null) {
 
-
+                if (new BigDecimal(statement.getAmount()).compareTo(fromAmount) == 1 &&
+                        toAmount.compareTo(new BigDecimal(statement.getAmount())) == 1) {
+                    filteredList.add(statement);  //TODO please think about all exceptions that might occure there
+                }
+            } else if (fromAmount != null && fromDate != null) {
+                DateFormat formatter = new SimpleDateFormat("dd.MM.yyyy"); //TODO move me to util method
+                Date date = formatter.parse(statement.getDateField());
+                if (date.before(toDate) && (date.after(fromDate) || date.equals(fromDate)) &&
+                        new BigDecimal(statement.getAmount()).compareTo(fromAmount) == 1 &&
+                        toAmount.compareTo(new BigDecimal(statement.getAmount())) == 1) { //TODO describe it
+                    filteredList.add(statement);  //TODO please think about all exceptions that might occure there
+                }
             }
 
 
--- src/main/java/nagarro/jpa/entity/StatementQuery.java
+++ src/main/java/nagarro/jpa/entity/StatementQuery.java
@@ -1,17 +1,25 @@
 package nagarro.jpa.entity;
 
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.math.BigDecimal;
 import java.util.Date;
 
 @Data
 public class StatementQuery {
 
+    @NotBlank(message = "Account ID is mandatory")
     public String accountId;
 
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     public Date fromDate;
 
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     public Date toDate;
 
     public BigDecimal fromBalance;
